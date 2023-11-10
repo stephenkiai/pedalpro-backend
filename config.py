7f4691bd06244e31a5198e64fdb50d7a22696fa1
stephenkiai <stephenkiai23@gmail.com>
2023-11-11 02:22:04 +0300

    db setup

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 class Config:
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'mysql://username:password@localhost/dbname')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'abcdefgh1234567890')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'postgres://pgqulwdclzedha:48eb104fc363ee75a99131a6b684b36758bc7fbaf87d0f8490652749b1edebd3@ec2-44-215-40-87.compute-1.amazonaws.com:5432/dc0egqt2llo2uo')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'your-jwt-secret-key')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'abcdefghijklmnopqrstuvwxyz1234567890')
 
 class DevelopmentConfig(Config):
     DEBUG = True
