7664164ce9055bc99aa2273f37fec6cba3710f8b
stephenkiai <stephenkiai23@gmail.com>
2023-11-12 01:25:14 +0300

    tweaked for heroku

--- app.py
+++ app.py
@@ -16,11 +16,12 @@ app.config.from_object('config.DevelopmentConfig')
 CORS(app, supports_credentials=True)
 
 #db.init_app(app)   
-#with app.app_context():
-#   db.create_all()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://pgqulwdclzedha:48eb104fc363ee75a99131a6b684b36758bc7fbaf87d0f8490652749b1edebd3@ec2-44-215-40-87.compute-1.amazonaws.com:5432/dc0egqt2llo2uo'
 db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
 
 app.config['JWT_SECRET_KEY'] = 'your_secret_key'
 jwt = JWTManager(app)
